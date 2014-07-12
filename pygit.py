a9ab15b863bc13f121d4ef1fd2eff74297eed3c3
Jordan Harris <jmh6657@rit.edu>
2014-07-12 19:00:21 -0400

    Changed Paths
    Updated from cwd() to __file__

--- pygit.py
+++ pygit.py
@@ -1,13 +1,14 @@
+#!/usr/bin/python
 import os
 import sys
 import subprocess
 
 def checkForConfig():
-	return os.path.isfile(os.getcwd()+"/config.txt")
+	return os.path.isfile(os.path.dirname(os.path.realpath(__file__))+"/config.txt")
 
 def readFromConfig():
 	if checkForConfig():
-		for line in open(os.getcwd()+"/config.txt"):
+		for line in open(os.path.dirname(os.path.realpath(__file__))+"/config.txt"):
 			line2 = line.replace('\n', '')
 			changeDirectory(line2)
 			git("pull")
@@ -15,8 +16,8 @@ def readFromConfig():
 	else:
 		print("The config file was not found. \nNow creating a new config file.")
 		try:
-			file = open(os.getcwd()+"/config.txt",'a')
-			file.close()
+			cfile = open(os.getcwd()+"/config.txt",'a')
+			cfile.close()
 		except:
 			print('Unable to create the file. \nPlease check your permissions.')
 			sys.exit(0)
