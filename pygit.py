35c79bd0c3f1802f122c82ae03b7f7a9545f1134
Jordan Harris <jmh6657@rit.edu>
2014-07-12 02:04:19 -0400

    Git Command
    Will now run the git command for each file in the config.txt

--- pygit.py
+++ pygit.py
@@ -1,12 +1,16 @@
 import os
 import sys
+import subprocess
 
 def checkForConfig():
 	return os.path.isfile(os.getcwd()+"/config.txt")
 
 def readFromConfig():
 	if checkForConfig():
-		return("Config file found.")
+		for line in open(os.getcwd()+"/config.txt"):
+			changeDirectory(line)
+			git("pull")
+
 	else:
 		print("The config file was not found. \nNow creating a new config file.")
 		try:
@@ -15,6 +19,13 @@ def readFromConfig():
 		except:
 			print('Unable to create the file. \nPlease check your permissions.')
 			sys.exit(0)
+
+def changeDirectory(path):
+	os.chdir(path)
+	return subprocess.call("ls")
+
+def git(*args):
+	return subprocess.check_call(['git'] + list(args))
 
 def main():
 	print(readFromConfig())
